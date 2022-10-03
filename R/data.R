bbb817ee7c86067cc8938e9b6fd86e4d2092d77a
duckduckquack <elogins34@tutanota.com>
2022-10-03 09:24:18 +0200

    Add data knapsack_objects

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#'Knapsack objects
+#'Data frame representing the items available to put in the knapsack
+#'@format
+#'   \describe{
+#'      \item{v}{values for the items}
+#'      \item{w}{weights for the items}}
+"knapsack_objects"
